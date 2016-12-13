4e8722c9528dbb205233cc486f52dfc6dd87b15d
xiongchaofs <xiongchaofs@163.com>
2016-12-14 00:00:05 +0800

    fdsfs

---
+++ src/main/java/com/fantasi/xdnphb/XCTest.java
@@ -0,0 +1,14 @@
+package com.fantasi.xdnphb;
+
+/**
+ * Hello world!
+ *
+ */
+public class XCTest 
+{
+	
+	public static void test() {
+		System.out.println("test 0555555555555555555=============");
+	}
+	
+}
